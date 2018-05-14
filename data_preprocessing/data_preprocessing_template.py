b679f0b24746fa28d7eac1cad4b14d9ffae944dd
nishant Khanna <nishantkhanna87@gmail.com>
2018-05-14 20:51:26 +0530

    Starting Work

---
+++ data_preprocessing/data_preprocessing_template.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 14 20:41:57 2018
+
+@author: nishantkhanna
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
