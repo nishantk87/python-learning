0b7c13a18f3004465e1d23c1959a94d7ffc0796a
Nishant Khanna <nishantkhanna87@gmail.com>
2018-05-15 12:33:52 +0530

    no message

--- data_preprocessing/data_preprocessing_template.py
+++ data_preprocessing/data_preprocessing_template.py
@@ -18,3 +18,10 @@ Import Dataset
 #dataset = pd.read_csv("/Users/nishantkhanna/Desktop/Machine Learning A-Z/Part 1 - Data Preprocessing/Data.csv")
 dataset = pd.read_csv("C:\\Users\\nkhanna\\Desktop\\Machine_Learning_AZ_Template_Folder\\Part 1 - Data Preprocessing\\Data.csv")
 X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,3].values
+
+#Missing data imputation
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = "NaN", strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:,1:3])
+X[:,1:3] = imputer.transform(X[:,1:3])
