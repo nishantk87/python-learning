38879d17b8a4b98a1a4e362a6329162b902aef0c
Nishant Khanna <nishantkhanna87@gmail.com>
2018-05-21 11:41:34 +0530

    no message

--- data_preprocessing/data_preprocessing_template.py
+++ data_preprocessing/data_preprocessing_template.py
@@ -25,3 +25,14 @@ from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = "NaN", strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[:,1:3])
 X[:,1:3] = imputer.transform(X[:,1:3])
+
+# Encode Categorical Data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+labelEncoder_X = LabelEncoder()
+X[:,0] = labelEncoder_X.fit_transform(X[:,0])
+oneHotEncoder = OneHotEncoder(categorical_features= [0])
+X = oneHotEncoder.fit_transform(X).toarray()
+
+labelEncoder_Y = LabelEncoder()
+y = labelEncoder_Y.fit_transform(y)
