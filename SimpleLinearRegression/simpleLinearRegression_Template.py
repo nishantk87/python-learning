2573a2bb466357a8a3c5107151c58571f372bea7
Nishant Khanna <nishantkhanna87@gmail.com>
2018-05-21 15:38:17 +0530

    no message

--- SimpleLinearRegression/simpleLinearRegression_Template.py
+++ SimpleLinearRegression/simpleLinearRegression_Template.py
@@ -25,3 +25,33 @@ y = dataset.iloc[:,1].values
 from sklearn.cross_validation import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+#Fiting Simple Linear Regression to training Set
+
+from sklearn.linear_model import LinearRegression
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting Test Set Results
+
+y_pred = regressor.predict(X_test)
+
+#Visualizing Training Set Results
+
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary V/S Experience(Training)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+#Visualizing Test Set Results
+
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_test, regressor.predict(X_test), color = 'blue')
+plt.title('Salary V/S Experience(Test)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
