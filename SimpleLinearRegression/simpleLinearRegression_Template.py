16d8f046638818dd8178cf06bb9b52e3b88b4276
Nishant Khanna <nishantkhanna87@gmail.com>
2018-05-21 14:39:24 +0530

    no message

--- SimpleLinearRegression/simpleLinearRegression_Template.py
+++ SimpleLinearRegression/simpleLinearRegression_Template.py
@@ -2,6 +2,26 @@
 """
 Created on Mon May 21 13:13:51 2018
 
-@author: nkhanna
+@author: nishantkhanna
 """
 
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""
+Import Dataset
+
+"""
+
+#dataset = pd.read_csv("/Users/nishantkhanna/Desktop/Machine Learning A-Z/Part 1 - Data Preprocessing/Data.csv")
+dataset = pd.read_csv("C:\\Users\\nkhanna\\Desktop\\Python-Learning\\SimpleLinearRegression\\Salary_Data.csv")
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,1].values
+
+#Splitting data to test and training
+
+from sklearn.cross_validation import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
